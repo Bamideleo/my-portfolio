ed85601d4a4349dc7098cd52aa697eef78fd997d
Bamideleo <51209392+Bamideleo@users.noreply.github.com>
2021-11-21 14:32:20 +0100

    Update Project.js

--- src/Compontents/Project.js
+++ src/Compontents/Project.js
@@ -19,8 +19,8 @@ function Project() {
                                 <img src={item.img} alt="img"/>
                                 </a>
                                 <p>{item.title}</p>
-                                 <h5>Password: {item.password}</h5>
                                  <h4>Username: {item.username}</h4>
+                                 <h5>Password: {item.password}</h5>
                                <div className="item">
                                    <div className="itemsGit">
                                    <a href={item.gitlink} target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square fa-2x"></i></a>
