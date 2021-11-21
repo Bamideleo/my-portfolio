be33bb9f2699d799d123e4961c0ec1d5048bf3e2
Bamideleo <51209392+Bamideleo@users.noreply.github.com>
2021-11-21 13:37:18 +0100

    Update About.js

--- src/Compontents/About.js
+++ src/Compontents/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Title from './Title';
 import {DataConsumer} from '../context/Context';
-import MyPd from  '../images/bb.jpg'
+import MyPd from  '../images/mypassport.png'
 
 
 function About() {
